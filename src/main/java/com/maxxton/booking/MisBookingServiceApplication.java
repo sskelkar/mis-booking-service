e177fc1cd8a796ebe3fd1d36c60ebdf239925c71
Sojjwal Kelkar <s.kelkar@maxxton.com>
2016-07-19 12:05:33 +0530

    init commit

---
+++ src/main/java/com/maxxton/booking/MisBookingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.maxxton.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisBookingServiceApplication.class, args);
+	}
+}
